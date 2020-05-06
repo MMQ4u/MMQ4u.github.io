2586d24d141558838304bbf45545e539fd40b6c7
JOJosephHassan01 <yussif2002@gmail.com>
2020-05-06 14:40:51 +0300

    edit index file

--- Yoseph/JS/Main.js
+++ Yoseph/JS/Main.js
@@ -1,6 +1,7 @@
 var m_0 = document.getElementById("m_0");
 var m_0_C = m_0.getContext("2d");
 m_0_C.strokeStyle = "rgba(226, 226, 226, 1)";
+m_0_C.lineWidth = 1;
 m_0_C.strokeRect(50, 20, 20, 20);
 m_0_C.strokeRect(30, 50, 50, 50);
 m_0_C.strokeRect(100, 20, 20, 20);
