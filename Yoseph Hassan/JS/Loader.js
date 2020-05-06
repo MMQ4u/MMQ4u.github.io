2f6efd30a74e17534de42b699fc0a803b17c2c0c
JOJosephHassan01 <yussif2002@gmail.com>
2020-05-06 14:54:17 +0300

    edit

---
+++ Yoseph Hassan/JS/Loader.js
@@ -0,0 +1,7 @@
+var l = document.getElementById("l");
+    m_1 = document.getElementById("m_1");
+setTimeout(m_1L, 10000);
+function m_1L() {
+    l.style.display = "none";
+    m_1.style.display = "block";
+}
