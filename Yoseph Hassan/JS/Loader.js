2fcb09d276d2b3cb2da79d6d0caa5e9b4b7a389c
JOJosephHassan01 <yussif2002@gmail.com>
2020-05-06 15:25:22 +0300

    edit

--- Yoseph Hassan/JS/Loader.js
+++
@@ -1,7 +0,0 @@
-var l = document.getElementById("l");
-    m_1 = document.getElementById("m_1");
-setTimeout(m_1L, 10000);
-function m_1L() {
-    l.style.display = "none";
-    m_1.style.display = "block";
-}
