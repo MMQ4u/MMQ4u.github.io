264c7f90324a64062a2c6459e865bc1219563013
JOJosephHassan01 <yussif2002@gmail.com>
2020-06-07 17:12:27 +0300

    web
    Co-Authored-By: JO <joyosephhassan@users.noreply.github.com>

---
+++ Yoseph/JS/Section0.js
@@ -0,0 +1,12 @@
+function s0_0_2_0FV() {
+    document.getElementById("s0_0_2_0").style.boxShadow = "";
+}
+function s0_0_2_0FO() {
+    document.getElementById("s0_0_2_0").style.boxShadow = "";
+}
+function s0_0_2_1FV() {
+    document.getElementById("s0_0_2_1").style.boxShadow = ;
+}
+function s0_0_2_1FO() {
+    document.getElementById("s0_0_2_1").style.boxShadow = ;
+}
