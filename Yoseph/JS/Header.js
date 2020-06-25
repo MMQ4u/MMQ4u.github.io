063995a04e7cf75de49cf2b83ad7317d9338bed7
JOJosephHassan01 <yussif2002@gmail.com>
2020-06-25 10:35:05 +0300

    ثيهف

--- Yoseph/JS/Header.js
+++ Yoseph/JS/Header.js
@@ -1,23 +1,20 @@
-var open = 0;
-function h_1_1_0FC() {
-    open++;
-    if (open == 1) {
-        document.getElementById("h_1_1_1").style.display = "block";
-    } else if (open == 2) {
-        open = 0;
-        document.getElementById("h_1_1_1").style.display = "none";
-    }
-    console.log(open);
-}
 window.onscroll = function() {h_1FS()};
 function h_1FS() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if ((document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) && window.innerWidth > 1030) {
         document.getElementById("h_1").style.position = "fixed";
+        document.getElementById("h_1").style.bottom = "auto";
         document.getElementById("h_1").style.top = "0";
         document.getElementById("h_1").style.backgroundColor = "rgba(255, 255, 255, 1)";
         document.getElementById("h_1").style.boxShadow = "0 3px 7px rgba(26, 30, 30, 0.3)";
         document.getElementById("h_1").style.width = "100%";
         document.getElementById("h_2").style.display = "block";
+        document.getElementById("h_1_1").style.display = "none";
+    } else if (window.innerWidth < 1125) {
+        document.getElementById("h_1").style.position = "fixed";
+        document.getElementById("h_1").style.bottom = "0";
+        document.getElementById("h_1").style.top = "auto";
+        document.getElementById("h_2").style.display = "none";
+        document.getElementById("h_1_1").style.display = "block";
     } else {
         document.getElementById("h_1").style.position = "static";
         document.getElementById("h_1").style.boxShadow = "none";
--- Yoseph/JS/Section2.js
+++ Yoseph/JS/Section2.js
@@ -69,21 +69,20 @@ var lm1 = [
     ],
     lmd = [
         document.getElementById("s2_0_2_0_1_1_1_5"),
+        document.getElementById("s2_0_2_0_1_1_2_3"),
         document.getElementById("s2_0_2_0_1_1_2_4"),
         document.getElementById("s2_0_2_0_1_1_2_5"),
         document.getElementById("s2_0_2_0_1_1_3_4"),
         document.getElementById("s2_0_2_0_1_1_3_5"),
         document.getElementById("s2_0_2_0_1_1_4_4"),
         document.getElementById("s2_0_2_0_1_1_4_5"),
+        document.getElementById("s2_0_2_0_1_1_5_1"),
         document.getElementById("s2_0_2_0_1_1_5_2"),
         document.getElementById("s2_0_2_0_1_1_5_3"),
         document.getElementById("s2_0_2_0_1_1_5_4"),
         document.getElementById("s2_0_2_0_1_1_5_5"),
-        document.getElementById("s2_0_2_0_1_1_6_2"),
-        document.getElementById("s2_0_2_0_1_1_6_3"),
         document.getElementById("s2_0_2_0_1_1_6_4"),
         document.getElementById("s2_0_2_0_1_1_6_5"),
-        document.getElementById("s2_0_2_0_1_1_7_1"),
         document.getElementById("s2_0_2_0_1_1_7_2"),
         document.getElementById("s2_0_2_0_1_1_7_3"),
         document.getElementById("s2_0_2_0_1_1_7_4"),
@@ -407,13 +406,13 @@ function girlf() {
 function boygirlm() {
     l1_PB = Number(lm1[0].value) + Number(lm1[1].value) + Number(lm1[2].value) + Number(lm1[3].value) + Number(lm1[4].value) + Number(lm1[5].value) + Number(lm1[6].value) + Number(lm1[7].value) + Number(lm1[8].value);
     l1_NB = (l1_PB / 9);
-    l2_PB = Number(lm2[0].value) + Number(lm2[1].value) + Number(lm2[2].value) + Number(lm2[3].value) + Number(lm2[4].value) + Number(lm2[5].value) + Number(lm2[6].value);
+    l2_PB = Number(lm2[0].value) + Number(lm2[1].value) + Number(lm2[2].value) + Number(lm2[3].value) + Number(lm2[4].value) + Number(lm2[6].value) + Number(lm2[7].value);
     l2_NB = (l2_PB / 7);
     l12_PB = l1_NB + l2_NB;
     l12_NB = l12_PB;
-    l3_PB = Number(lm3[0].value) + Number(lm3[1].value) + Number(lm3[2].value) + Number(lm3[3].value) + Number(lm3[4].value);
-    l3_NB = (l3_PB / 5);
-    l4_PB = Number(lm4[0].value) + Number(lm4[1].value) + Number(lm4[2].value) + Number(lm4[3].value) + Number(lm4[4].value);
+    l3_PB = Number(lm3[0].value) + Number(lm3[1].value) + Number(lm3[2].value) + Number(lm3[3].value) + Number(lm3[4].value) + Number(lm3[6].value);
+    l3_NB = (l3_PB / 6);
+    l4_PB = Number(lm4[0].value) + Number(lm4[1].value) + Number(lm4[3].value) + Number(lm4[4].value) + Number(lm4[6].value);
     l4_NB = (l4_PB / 5);
     l34_PB = l3_NB + l4_NB;
     l34_NB = l34_PB;
