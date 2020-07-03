a121a592ca49b65ea73aabb9d4d28c6d3b4eb23e
JO <44245689+JOYosephHassan@users.noreply.github.com>
2020-07-03 19:19:13 +0300

    Update Section2.js

--- Yoseph/JS/Section2.js
+++ Yoseph/JS/Section2.js
@@ -412,7 +412,7 @@ function boygirlm() {
     l12_NB = l12_PB;
     l3_PB = Number(lm3[0].value) + Number(lm3[1].value) + Number(lm3[2].value) + Number(lm3[3].value) + Number(lm3[4].value) + Number(lm3[6].value);
     l3_NB = (l3_PB / 6);
-    l4_PB = Number(lm4[0].value) + Number(lm4[1].value) + Number(lm4[3].value) + Number(lm4[4].value) + Number(lm4[6].value);
+    l4_PB = Number(lm4[0].value) + Number(lm4[1].value) + Number(lm4[2].value) + Number(lm4[3].value) + Number(lm4[4].value);
     l4_NB = (l4_PB / 5);
     l34_PB = l3_NB + l4_NB;
     l34_NB = l34_PB;
